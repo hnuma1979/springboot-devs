747adb6b5234a3e234c45f56204ce22dc3441ebd
asahinadev <59528169+asahinadev@users.noreply.github.com>
2022-11-27 01:24:50 +0900

    メールを テンプレート対応

--- src/main/java/jp/mirageworld/spring/common/service/MailSenderService.java
+++ src/main/java/jp/mirageworld/spring/common/service/MailSenderService.java
@@ -1,71 +1,183 @@
 package jp.mirageworld.spring.common.service;
 
+import java.util.Locale;
+
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.MailSender;
-import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 import jp.mirageworld.spring.common.config.properties.MailProperties;
+import jp.mirageworld.spring.common.model.Users;
 import lombok.extern.slf4j.Slf4j;
 
+/**
+ * メール送信サービス。
+ */
 @Slf4j
 @Service
 public class MailSenderService {
     
-    final MailSender  mailSender;
+    final  JavaMailSender mailSender;
     final MailProperties mailProperties;
+    final TemplateEngine templateEngine;
+
+    final Locale locale = Locale.JAPAN;
     
+    /**
+     * 標準コンストラクタ。
+     * 
+     * 直接呼ぶ必要はない
+     * 
+     * @param mailSender
+     * @param mailProperties
+     * @param templateEngine
+     */
+    @Deprecated
     @Autowired
     public MailSenderService(
-        MailSender      mailSender,
-        MailProperties  mailProperties
+        JavaMailSender  mailSender,
+        MailProperties  mailProperties,
+        TemplateEngine  templateEngine
     ) {
         this.mailSender         = mailSender;
         this.mailProperties     = mailProperties;
+        this.templateEngine     = templateEngine;
     }
 
     /**
-     * メール送信処理
+     * テキスト形式のメールを送信.
      * 
-     * @param to
-     * @param subject
-     * @param body
-     * @return
-     * @throws Exception
+     * @param to            送信先（TO）
+     * @param subject       件名
+     * @param body          本文（TEXT)
+     * @return              正常／異常
+     * @throws Exception    実行時例外
+     * @see #send(String, String, String, String, String)
      */
-    public boolean send(String to, String subject, String body) throws Exception {
-        return send(to, null, subject, body);
+    public boolean sendText(String to, String subject, String body) throws Exception {
+        return send(to, null, subject, body, null);
     }
 
     /**
-     * メール送信処理
+     * テキスト形式のメールを送信.
      * 
-     * @param to
-     * @param cc
-     * @param subject
-     * @param body
-     * @return
-     * @throws Exception
+     * @param to            送信先（TO）
+     * @param cc            送信先（CC）
+     * @param subject       件名
+     * @param body          本文（TEXT)
+     * @return              正常／異常
+     * @throws Exception    実行時例外
+     * @see #send(String, String, String, String, String)
      */
-    public boolean send(String to,String cc, String subject, String body) throws Exception {
+    public boolean sendText(String to,String cc, String subject, String body) throws Exception {
+        return send(to, cc, subject, body, null);
+    }
+
+    /**
+     * テキスト形式のメールを送信.
+     * 
+     * @param to            送信先（TO）
+     * @param subject       件名
+     * @param body          本文（TEXT)
+     * @param html          本文（HTML)
+     * @return              正常／異常
+     * @throws Exception    実行時例外
+     * @see #send(String, String, String, String, String)
+     */
+    public boolean send(String to, String subject, String body, String html) throws Exception {
+        return send(to, null, subject, body, html);
+    }
+
+    /**
+     * テンプレート使用のメールを送信.
+     * 
+     * @param to            送信先（TO）
+     * @param cc            送信先（CC）
+     * @param subject       件名
+     * @param templateKey   テンプレートキー
+     * @param Users         {@link Users}
+     * @return              正常／異常
+     * @throws Exception    実行時例外
+     * @see #send(String, String, String, String, String)
+     */
+    public boolean sendTtemplate(String to, String subject, String templateKey, Users users) throws Exception {
+        return sendTtemplate(to,null,subject,templateKey, users);
+    }   
+
+    /**
+     * テンプレート使用のメールを送信.
+     * 
+     * @param to            送信先（TO）
+     * @param cc            送信先（CC）
+     * @param subject       件名
+     * @param templateKey   テンプレートキー
+     * @param Users         {@link Users}
+     * @return              正常／異常
+     * @throws Exception    実行時例外
+     * @see #send(String, String, String, String, String)
+     */
+    public boolean sendTtemplate(String to,String cc, String subject, String templateKey, Users users) throws Exception {
+         Context ctx = new Context();
+        ctx.setVariable("users"  , users);
+
+         String text = "";
+         String html = "";
+        
+        try {
+            text = this.templateEngine.process("_mail/"+templateKey+".txt", ctx);;
+        } catch (Exception e) {
+            log.debug(e.getMessage());
+        }
+        
+        try {
+            html = this.templateEngine.process("_mail/"+templateKey+".html", ctx);;
+        } catch (Exception e) {
+            log.debug(e.getMessage());
+        }
+
+        return send(to, cc, subject, text, html);
+    }
+
+    /**
+     * テキスト形式のメールを送信.
+     * 
+     * @param to            送信先（TO）
+     * @param cc            送信先（CC）
+     * @param subject       件名
+     * @param body          本文（TEXT)
+     * @param html          本文（HTML)
+     * @return              正常／異常
+     * @throws Exception    実行時例外
+     */
+    boolean send(String to,String cc, String subject, String body, String html) throws Exception {
         log.debug("send(from,cc,sub,body) : START");
         try {
-            SimpleMailMessage mailMessage = new SimpleMailMessage();
-
-            mailMessage.setFrom(mailProperties.getFrom()); // 送信元メールアドレス
-            mailMessage.setTo(to);
-            if (cc != null) {
-                mailMessage.setCc(cc);
+            if (!mailProperties.isSend()) {
+                log.debug("sub => {}, body => {}, html => {}", subject, body, html);
+                return true;
             }
-            mailMessage.setSubject(subject);
-            mailMessage.setText(body);
+            MimeMessage message         = mailSender.createMimeMessage();
+            MimeMessageHelper helper    = new MimeMessageHelper(message, true);
 
-            if (mailProperties.isSend()) {
-                mailSender.send(mailMessage);
+            helper.setFrom(mailProperties.getFrom()); // 送信元メールアドレス
+            helper.setTo(to);
+            if (StringUtils.hasText(cc))  helper.setCc(cc);
+            helper.setSubject(subject);
+            if ( html == null ) {
+                helper.setText(body);
             } else {
-                log.debug("sub => {}, body => {}", subject, body);
+                helper.setText(body, html);
             }
+
+            mailSender.send(message);
             return true;
+
         } catch (Exception e) {
             log.error(e.getMessage(), e);
             return false;
--- src/test/java/jp/mirageworld/spring/common/service/MailSenderServiceTest.java
+++ src/test/java/jp/mirageworld/spring/common/service/MailSenderServiceTest.java
@@ -1,10 +1,15 @@
 package jp.mirageworld.spring.common.service;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Optional;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import jp.mirageworld.spring.common.config.properties.MailProperties;
+import jp.mirageworld.spring.common.model.Users;
 import lombok.SneakyThrows;
 
 @SpringBootTest
@@ -16,14 +21,23 @@ public class MailSenderServiceTest {
     @Autowired
     MailSenderService mailSenderService;
 
+    @Autowired
+    UsersService usersService;
+
+    static final String SUBJECT  = "TEST";
+    static final String BODY     = "送信テスト";
 
     @Test
     @SneakyThrows
-    public void test() {
-        mailSenderService.send(
-            mailProperties.getFrom(), 
-            "TEST", 
-            "送信テスト"
-        );
+    public void testSendText() {
+        mailSenderService.sendText(mailProperties.getFrom(), mailProperties.getFrom(), SUBJECT, BODY);
+    }
+
+    @Test
+    @SneakyThrows
+    public void testSendTemplateSignup() {
+        Optional<Users> users = usersService.findById(1);
+        assertTrue(users.isPresent(), "システムアカウント未登録");
+        mailSenderService.sendTtemplate(mailProperties.getFrom(), mailProperties.getFrom(), SUBJECT, "signup",users.get());
     }
 }
